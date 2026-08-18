d40b1c4079c4a3775703e4715d1aa5042c717a9b
Hemed Lungo <86722789+Heed725@users.noreply.github.com>
2026-08-18 10:00:32 +0300

    Add QGIS processing provider

---
+++ terraclimate_downloader/plugin.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import os
+
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import QAction
+from qgis.core import QgsApplication, QgsProcessingProvider
+
+from .terraclimate_algorithm import TerraClimateDownloadAlgorithm
+
+
+class TerraClimateProvider(QgsProcessingProvider):
+    def id(self):
+        return "terraclimate_downloader"
+
+    def name(self):
+        return "TerraClimate Downloader"
+
+    def longName(self):
+        return self.name()
+
+    def icon(self):
+        return QIcon(os.path.join(os.path.dirname(__file__), "icon.svg"))
+
+    def loadAlgorithms(self):
+        self.addAlgorithm(TerraClimateDownloadAlgorithm())
+
+
+class TerraClimateDownloaderPlugin:
+    def __init__(self, iface):
+        self.iface = iface
+        self.provider = None
+        self.action = None
+
+    def initGui(self):
+        self.provider = TerraClimateProvider()
+        QgsApplication.processingRegistry().addProvider(self.provider)
+
+        icon = QIcon(os.path.join(os.path.dirname(__file__), "icon.svg"))
+        self.action = QAction(icon, "TerraClimate Downloader", self.iface.mainWindow())
+        self.action.setToolTip("Open the TerraClimate Downloader processing algorithm")
+        self.action.triggered.connect(self.run)
+        self.iface.addPluginToMenu("&TerraClimate Downloader", self.action)
+        self.iface.addToolBarIcon(self.action)
+
+    def unload(self):
+        if self.action is not None:
+            self.iface.removePluginMenu("&TerraClimate Downloader", self.action)
+            self.iface.removeToolBarIcon(self.action)
+            self.action.deleteLater()
+            self.action = None
+        if self.provider is not None:
+            QgsApplication.processingRegistry().removeProvider(self.provider)
+            self.provider = None
+
+    def run(self):
+        self.iface.mainWindow().show()
+        import processing
+        processing.execAlgorithmDialog("terraclimate_downloader:download_terraclimate")
